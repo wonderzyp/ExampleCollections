7fb6d12ea7043a72a541bee717913e8777e6a91c
zyp <wonderzyp@qq.com>
2023-01-30 14:54:19 +0800

    BoardCast Demo: force offline

---
+++ androidDemo/BoardcastDemo/app/src/main/java/com/example/boardcastdemo/BaseAcitvity.java
@@ -0,0 +1,64 @@
+package com.example.boardcastdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseAcitvity extends AppCompatActivity {
+    private ForceOfflineReceiver receiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.boardcastdemo.FORCE_OFFLINE");
+        receiver = new ForceOfflineReceiver();
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (receiver!=null){
+            unregisterReceiver(receiver);
+            receiver = null;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+
+    class ForceOfflineReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Warning");
+            builder.setMessage("You're forced to be offline");
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    ActivityCollector.finishAll();
+                    Intent tempIntent = new Intent(context, LoginActivity.class);
+                    context.startActivity(tempIntent);
+                }
+            });
+            builder.show();
+        }
+    }
+}
---
+++ androidDemo/BoardcastDemo/app/src/main/java/com/example/boardcastdemo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.boardcastdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends BaseAcitvity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button forceOffline = (Button) findViewById(R.id.force_offline);
+        forceOffline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.example.boardcastdemo.FORCE_OFFLINE");
+                sendBroadcast(intent);
+            }
+        });
+    }
+}
